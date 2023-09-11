58dcdee68927e2ee8104b537dc84f926e8793a9f
zafri120 <135434921+zafri120@users.noreply.github.com>
2023-09-11 09:30:14 +0000

    basics

---
+++ basic/datatypesummary.js
@@ -0,0 +1,15 @@
+// let youtubeName = "Taraq Mehta Ka Sidha Chashma"
+// aotherName = youtubeName
+// anotherNAme = "Jetha Laal"
+// console.log(anotherNAme)
+// console.log(youtubeName)
+let userOne =   {
+    email : "r.rizwan.hafeez@gmai.com",
+    password : "Tujhe q Batao"
+}
+let userTwo = userOne
+userTwo.email = "askrizwanhafeez@gmail.com"
+userTwo.password = "Srf tujhe hi bataonga"
+console.log(userTwo);
+
+console.log(userOne);
