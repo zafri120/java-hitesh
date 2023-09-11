b45b45dbfa37832b38e42a2bff357bf5d15bc38a
zafri120 <135434921+zafri120@users.noreply.github.com>
2023-09-11 10:25:39 +0000

    math and number

---
+++ basic/06_string.js
@@ -0,0 +1,4 @@
+ const name = "Rizwan"
+ const repoCount = 50
+ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+ const gameName = new String('Rizwanhcc')
---
+++ basic/07_mathandnumber.js
@@ -0,0 +1,15 @@
+// const score = 400
+// console.log(score);
+// const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+// const otherNumber = 123.899034
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString(`en-US`))
+
+// console.log(Math)
+console.log(Math.random)
+console.log(Math.random())
